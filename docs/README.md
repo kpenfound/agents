d58661210f7ace88cf45700bd414f3076fdfc91c
kpenfound <kyle@dagger.io>
2025-04-10 17:07:19 -0400

    add firecrawl option
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/README.md
+++ docs/README.md
@@ -14,8 +14,18 @@ prompt "how do I avoid caching a certain step in my pipeline?"
 
 The main function `prompt` is the agent to prompt for specific information about the docs site.
 
-To point at a documentation website other than docs.dagger.io, you can override the default values from the constructor by running:
+### Point to any website
+
+To point at a website other than docs.dagger.io that supports `llms.txt` and `llms-full.txt`, you can override the default values from the constructor by running:
 
 ```
 docs --base-url https://docs.firecrawl.dev | prompt "how do i create a firecrawl client?"`
+```
+
+### Give the agent additional crawling capabilities
+
+To give the agent capabilities to explore beyond the provided `llms-full.txt`, pass in a firecrawl.dev API key:
+
+```
+docs --firecrawl-key $FIRECRAWL_KEY | prompt "how do i configure dagger to work with docker model runner?"
 ```
--- docs/main.go
+++ docs/main.go
@@ -4,12 +4,14 @@ package main
 
 import (
 	"context"
+	"dagger/docs/internal/dagger"
 )
 
 type Docs struct {
-	BaseURL string
-	Txt     string
-	Full    string
+	BaseURL      string
+	Txt          string
+	Full         string
+	FirecrawlKey *dagger.Secret
 }
 
 func New(
@@ -19,11 +21,14 @@ func New(
 	txt string,
 	//+default="llms-full.txt"
 	full string,
+	//+optional
+	firecrawlKey *dagger.Secret,
 ) Docs {
 	return Docs{
-		BaseURL: baseURL,
-		Txt:     txt,
-		Full:    full,
+		BaseURL:      baseURL,
+		Txt:          txt,
+		Full:         full,
+		FirecrawlKey: firecrawlKey,
 	}
 }
 
@@ -38,6 +43,13 @@ func (m *Docs) Prompt(ctx context.Context, prompt string) (string, error) {
 		WithFileInput("llmsfull", full, "The entire documentation site expanded as a single markdown file").
 		WithStringInput("prompt", prompt, "A prompt for information related to the information contained in the files").
 		WithUtilsInput("utils", utils, "Utility tools for searching for content in the docs files")
+
+	if m.FirecrawlKey != nil {
+		firecrawl := dag.FirecrawlDag(m.FirecrawlKey)
+		env = env.
+			WithStringInput("base", m.BaseURL, "the base URL of the documentation website for use with firecrawl").
+			WithFirecrawlDagInput("firecrawl", firecrawl, "a tool to use if you cannot find the required information in $llm or $llmsfull. Use $base and paths in $llm to scrape pages. Do not use crawl, only map and scrape.")
+	}
 
 	return dag.LLM().
 		WithEnv(env).
